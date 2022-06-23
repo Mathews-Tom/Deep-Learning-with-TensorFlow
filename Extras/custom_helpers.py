7dc8a652d8cc585ad27e4cdb8a4d1358553bf6e8
Tom Mathews <tommathews2007@gmail.com>
2022-06-23 15:29:11 +0530

    Add function for evaluating Time Series Predictions

--- Extras/custom_helpers.py
+++ Extras/custom_helpers.py
@@ -299,6 +299,7 @@ def calculate_results(y_true, y_pred):
                      "f1": model_f1}
     return model_results
 
+
 # Create a function to plot time series data
 def plot_time_series(timesteps, values, format='.', start=0, end=None, label=None):
     """
@@ -319,3 +320,40 @@ def plot_time_series(timesteps, values, format='.', start=0, end=None, label=Non
     if label:
         plt.legend(fontsize=14) # make label bigger
     plt.grid(True)
+
+
+def mean_absolute_scaled_error(y_true, y_pred):
+    """
+    Implement MASE (assuming no seasonality of data).
+
+    # MASE implemented courtesy of sktime - https://github.com/alan-turing-institute/sktime/blob/ee7a06843a44f4aaec7582d847e36073a9ab0566/sktime/performance_metrics/forecasting/_functions.py#L16
+
+    """
+    mae = tf.reduce_mean(tf.abs(y_true - y_pred))
+
+    # Find MAE of naive forecast (no seasonality)
+    mae_naive_no_season = tf.reduce_mean(tf.abs(y_true[1:] - y_true[:-1])) # our seasonality is 1 day (hence the shifting of 1 day)
+
+    return mae / mae_naive_no_season
+
+
+def evaluate_time_series_preds(y_true, y_pred):
+    """
+    Evaluate Time Series Predictions.
+    """
+    # Make sure float32 (for metric calculations)
+    y_true = tf.cast(y_true, dtype=tf.float32)
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+    # Calculate various metrics
+    mae = tf.keras.metrics.mean_absolute_error(y_true, y_pred)
+    mse = tf.keras.metrics.mean_squared_error(y_true, y_pred) # puts and emphasis on outliers (all errors get squared)
+    rmse = tf.sqrt(mse)
+    mape = tf.keras.metrics.mean_absolute_percentage_error(y_true, y_pred)
+    mase = mean_absolute_scaled_error(y_true, y_pred)
+    
+    return {"mae": mae.numpy(),
+            "mse": mse.numpy(),
+            "rmse": rmse.numpy(),
+            "mape": mape.numpy(),
+            "mase": mase.numpy()}
